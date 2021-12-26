f82946375deac56a1dcf7dcac582ff68575946a8
nabitbol <abitbol.noam@protonmail.com>
2021-12-26 09:40:27 +0100

    feat(main) added some tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ using namespace NAMESPACE;
 template < typename T >
 void	display(const T &instance, int index) {
 	std::cout << instance[index];
+	std::cout << ' ';
 }
 
 template < typename T >
 void	iter(const T &instance, size_t size, void function(const T &, int)) {
 	for (size_t i = 0; i < size; i++) {
 		function(instance, i);
-		std::cout << ' ';
 	}
 	std::cout << std::endl;
 }
@@ -50,12 +50,18 @@ int	main(void) {
 
 	std::cout << std::endl;
 
+	std::cout << a.capacity() << std::endl;
+	std::cout << b.capacity() << std::endl;
+	std::cout << c.capacity() << std::endl;
+
+	std::cout << std::endl;
+
+	iter< ::vector<int> >(c, c.size(), &display);
+
 	//Call resize
 	a.resize(5, 400);
 	b.resize(10, 70000);
 	c.resize(9);
-
-	std::cout << std::endl;
 
 	std::cout << a.size() << std::endl;
 	std::cout << b.size() << std::endl;
@@ -63,7 +69,16 @@ int	main(void) {
 
 	std::cout << std::endl;
 
-	iter< ::vector<int> >(a, a.size(), &display);
+	std::cout << a.capacity() << std::endl;
+	std::cout << b.capacity() << std::endl;
+	std::cout << c.capacity() << std::endl;
+
+	iter< ::vector<int> >(c, c.size(), &display);
+
+	std::cout << std::endl;
+
+	// iter< ::vector<int> >(a, a.size(), &display);
+	// iter< ::vector<int> >(b, b.size(), &display);
 
 	std::cout << std::endl;
 
